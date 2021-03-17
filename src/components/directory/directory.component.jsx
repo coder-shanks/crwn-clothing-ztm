87085b844845ada7db58abb80f6aa0a109ff5ace
Shubham Tarade <coder.shanks@gmail.com>
2021-03-17 17:06:32 +0530

    update directory data files as per best practices

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
-import { sections } from './directory.data';
+import SECTIONS from './directory.data';
 import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor() {
     super();
 
-    this.state = { sections: sections };
+    this.state = { sections: SECTIONS };
   }
 
   render() {
