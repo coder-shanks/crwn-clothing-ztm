400c2721fde3a50f6358883fce5f7575e469e515
Shubham Tarade <coder.shanks@gmail.com>
2021-03-24 22:08:36 +0530

    exporting actionTypes to separate file & updating the app

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,7 @@
+import { UserActionTypes } from './user.types';
 // setCurrentUser => Action Creator
 // {type: '***', payload: *** } => Action
 export const setCurrentUser = (user) => ({
-  type: 'SET_CURRENT_USER',
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
