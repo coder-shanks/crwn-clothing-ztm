3ffe24e11d4eea6b91b7b0cd23fa31aadb86ab80
Shubham Tarade <coder.shanks@gmail.com>
2021-04-04 11:22:21 +0530

    convert homepage to styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
+//import './homepage.styles.scss';
 
 const HomePage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
