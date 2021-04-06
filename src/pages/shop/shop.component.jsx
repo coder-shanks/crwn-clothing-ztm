9dc40a7868192351f46a0812b53c0cbb233f1e62
Shubham Tarade <coder.shanks@gmail.com>
2021-04-06 20:26:14 +0530

    convert observable to promise pattern for collection snapshot

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,13 +27,11 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionMap = await convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionMap);
-        this.setState({ isLoading: false });
-      }
-    );
+    collectionRef.get().then((snapshot) => {
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionMap);
+      this.setState({ isLoading: false });
+    });
   }
 
   render() {
