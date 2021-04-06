414e637bcf16f5c2704ae54c224a03cfcab5bf55
Shubham Tarade <coder.shanks@gmail.com>
2021-04-06 12:58:43 +0530

    remove shop data, add null checks for collection as data is asynchronously fetched from firestore, add withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
+
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPage from '../collection/collection.component';
+
 import {
   convertCollectionsSnapshotToMap,
   firestore,
 } from '../../firebase/firebase.utils';
-import CollectionPage from '../collection/collection.component';
 
 import { updateCollections } from '../../redux/shop/shop.actions.js';
 
 import './shop.styles.scss';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = { isLoading: true };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -23,18 +31,29 @@ class ShopPage extends React.Component {
       async (snapshot) => {
         const collectionMap = await convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionMap);
+        this.setState({ isLoading: false });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { isLoading } = this.state;
+
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={isLoading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={isLoading} {...props} />
+          )}
         />
       </div>
     );
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,13 +9,13 @@ export const selectShopCollections = createSelector(
 );
 
 export const selectShopCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectShopCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectShopCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   )
 );
 
 export const selectShopCollectionsForPreview = createSelector(
   [selectShopCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
